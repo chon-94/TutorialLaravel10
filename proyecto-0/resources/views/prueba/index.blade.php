f80e0e41bffcd84b42002f978f76339443f52362
chon-94 <107455342+chon-94@users.noreply.github.com>
2024-04-15 23:53:23 -0500

    pruebaindex

--- proyecto-0/resources/views/prueba/index.blade.php
+++ proyecto-0/resources/views/prueba/index.blade.php
@@ -1,14 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Prueba') }}
+        </h2>
+    </x-slot>
 
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <h4>
+                        {{ __("You're logged in!") }}
+                    </h4>
+                    <h2>Formularios pruebas</h2>
+                    ya y ademas este es el pruebas formya y ademas este es el pruebas formya y ademas este es el pruebas ya y ademas este es el pruebas formya y ademas este es el pruebas f
+                    a y ademas este es el pruebas ya formya y ademas este es pruebasya y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas e
+                    ste es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el prue
+                    bas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas form     ya y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ad
+                                        ya y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas est
+e es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas form     ya y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas fa y ademas este es el pruebas form                     ya y ademas este es el pruebas form  y ademas este es el pruebas form                     ya y ademas este es el pruebas form                     ya y ademas este es el pruebas form                     ya y ademas este es el pruebas form                     ya y ademas este es el pruebas form                     ya y ademas este es el pruebas form                     ya y ademas este es el pruebas form                     ya y ademas este es el pruebas form                     ya y ademas este es el pruebas form                     ya y ademas este es el pruebas form                     ya y ademas este es el pruebas form 
+emas este es el pruebas f
+                    ya y ademas este es el pruebas form 
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
 
-<body>
-    hola cara cola
-</body>
-</html>
